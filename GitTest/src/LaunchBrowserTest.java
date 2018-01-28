d55aa8530dc4ccfab638c95426e44d16013ca67e
LavanyaJeejappa <lavanya.lav888@gmail.com>
2018-01-28 19:55:32 +0530

    first commit

---
+++ GitTest/src/LaunchBrowserTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowserTest {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.facebook.com");
+		/*driver.findElement(By.id("email")).sendKeys("lavanyajlav");
+		driver.findElement(By.id("pass")).sendKeys("lav888");*/
+		
+	}
+
+}
